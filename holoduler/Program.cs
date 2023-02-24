b5559186a1004cd082cbdf0bcc86a1a5b950c47f
Manabu Kikuchi <mkikuchi@imprex.co.jp>
2023-02-24 14:29:18 +0900

    updated.

--- holoduler/Program.cs
+++ holoduler/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 var userName = Environment.GetEnvironmentVariable("API_USERNAME")!;
 var password = Environment.GetEnvironmentVariable("API_PASSWORD")!;
 var baseUrl = Environment.GetEnvironmentVariable("API_URL")!;
+// サービスコンテナへ登録
 builder.Services.AddTransient<IDataService>(_ => new DataService(userName, password, baseUrl));
 // コントローラーを使用
 builder.Services.AddControllers();
@@ -22,10 +23,10 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-// 規則ルーティング（Conventional routing）から、Web API 2 の属性ルーティング （Attribute routing）へ変更
+// Web API 2 の属性ルーティング（Attribute routing）
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
-}); 
+});
 app.MapFallbackToFile("index.html"); ;
 app.Run();
